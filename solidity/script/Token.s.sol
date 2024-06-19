15a2aac76d654e12c79c8b742c9faf6c9e6c1dbc
ulyh <janeuly@pm.me>
2024-06-19 19:58:10 +0200

    Add tests smart contracts

---
+++ solidity/script/Token.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Token} from "../src/Token.sol";
+
+contract TokenScript is Script {
+    Token public token;
+
+    function run() public {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        token = new Token(50, "MyToken", "MTK");
+        console.log("balance: of sender", token.balanceOf(msg.sender));
+        console.log("totalSupply: ", token.totalSupply());
+        console.log("Token Symbol: ", token.symbol());
+        vm.stopBroadcast();
+    }
+}
