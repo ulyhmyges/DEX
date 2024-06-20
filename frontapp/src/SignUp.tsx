a64566a476551b08aecb7eedd09f37c9b30c118e
Malthus Ronaldo Djatche <djatchemalthus@gmail.com>
2024-06-20 10:18:52 +0200

    Avancement CRUD

--- frontapp/src/SignUp.tsx
+++ frontapp/src/SignUp.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { signup } from './services/UserService'; 
 
 const SignUp = () => {
     const [username, setUsername] = useState('');
@@ -10,16 +11,7 @@ const SignUp = () => {
     const handleSignUp = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:8080/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username, password })
-            });
-
-            if (!response.ok) {
-                throw new Error('Username already taken');
-            }
-
+            await signup(username, password);
             navigate('/login');
         } catch (err: any) {
             setError(err.message);
