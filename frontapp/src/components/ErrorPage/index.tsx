2e337f8abdb21948eec7ecb769b5f5af7706aa4b
ulyh <janeuly@pm.me>
2024-07-14 10:38:25 +0200

    Modified ErrorPage component

--- frontapp/src/components/ErrorPage/index.tsx
+++ frontapp/src/components/ErrorPage/index.tsx
@@ -1,19 +1,24 @@
-import React from "react";
-import {useRouteError} from "react-router-dom";
+import Container from "../Container";
+import "./ErrorPage.css";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    const error = useRouteError();
-    console.error(error);
+  const error = useRouteError();
+  console.error(error);
 
-    return (
-        <div id="error">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{"Error 404"}</i>
-            </p>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <div id="error">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p className="error">
+          <i>{"Error 404"}</i>
+        </p>
+      </div>
+      <Link to={"/home"}>Go to Home
+      </Link>
+    </Container>
+  );
+};
 
 export default ErrorPage;
