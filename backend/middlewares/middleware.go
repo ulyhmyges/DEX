7cef38f24daaf19670ab4a1e0760951eb3ba1539
Malthus Ronaldo Djatche <djatchemalthus@gmail.com>
2024-07-14 20:38:11 +0200

    avancement crud sur la branche feature/CRUD_utilisateur

--- backend/middlewares/middleware.go
+++ backend/middlewares/middleware.go
@@ -1,16 +1,17 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 )
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		// Handle preflight requests
+		log.Println("bonjour")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
@@ -19,3 +20,28 @@ func EnableCORS(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// package middleware
+
+// import (
+// 	"log"
+// 	"net/http"
+// )
+
+// func EnableCORS(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Access-Control-Allow-Origin", "*")
+// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+// 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+// 		// Handle preflight requests
+// 		if r.Method == http.MethodOptions {
+// 			w.WriteHeader(http.StatusOK)
+// 			return
+// 		}
+
+// 		log.Println("bonjour")
+
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
